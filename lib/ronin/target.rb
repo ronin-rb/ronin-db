7affc75c17162070a7c7a78c3e29f43832e7cc78
postmodern <postmodern.mod3@gmail.com>
2008-06-23 07:54:55 +0000

    * Added DataMapper 0.9 support.

---
+++ lib/ronin/target.rb
@@ -0,0 +1,40 @@
+#
+# Ronin - A decentralized repository for the storage and sharing of computer
+# security advisories, exploits and payloads.
+#
+# Copyright (c) 2007 Hal Brodigan (postmodern at users.sourceforge.net)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+
+require 'ronin/model'
+require 'ronin/platform'
+require 'ronin/arch'
+
+module Ronin
+  class Target
+
+    include Model
+
+    property :id, Serial
+
+    # Targeted platform
+    belongs_to :platform, :class_name => 'Ronin::Platform'
+
+    # Targeted architecture
+    belongs_to :arch, :class_name => 'Ronin::Arch'
+
+  end
+end
