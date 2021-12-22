ec2a4e587a04fb5d8b6c6224560751c264944f72
Postmodern <postmodern.mod3@gmail.com>
2021-12-22 02:45:09 -0800

    Changed the project name to ronin-db.

--- lib/ronin/database/migrations/arch.rb
+++ lib/ronin/database/migrations/arch.rb
@@ -1,20 +1,22 @@
 #
-# Copyright (c) 2006-2012 Hal Brodigan (postmodern.mod3 at gmail.com)
+# ronin-db - A common database library for managing and querying security data.
 #
-# This file is part of Ronin.
+# Copyright (c) 2006-2021 Hal Brodigan (postmodern.mod3 at gmail.com)
 #
-# Ronin is free software: you can redistribute it and/or modify
+# This file is part of ronin-db.
+#
+# ronin-db is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Ronin is distributed in the hope that it will be useful,
+# ronin-db is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Ronin.  If not, see <http://www.gnu.org/licenses/>.
+# along with ronin-db.  If not, see <https://www.gnu.org/licenses/>.
 #
 
 require 'ronin/database/migrations/migrations'
