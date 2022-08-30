ab8e5a257b1717a2875fdb6a46cab22b051e6dfb
Postmodern <postmodern.mod3@gmail.com>
2022-08-30 16:30:16 -0700

    Added `Ronin::DB::CLI::DatabaseCommand` (closes #30).

---
+++ lib/ronin/db/cli/database_command.rb
@@ -0,0 +1,70 @@
+#
+# ronin-db - A common database library for managing and querying security data.
+#
+# Copyright (c) 2006-2022 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# ronin-db is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-db is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-db.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'ronin/db/cli/command'
+require 'ronin/db/cli/uri_methods'
+require 'ronin/db'
+
+module Ronin
+  module DB
+    class CLI
+      #
+      # Base class for all commands that access the database.
+      #
+      class DatabaseCommand < Command
+
+        include URIMethods
+
+        option :db, value: {
+                      type:    DB.config.keys,
+                      default: :default,
+                      usage:   'NAME'
+                    },
+                    desc: 'The database to connect to'
+
+        option :db_uri, value: {
+                          type:  String,
+                          usage: 'URI'
+                        },
+                        desc: 'The database URI to connect to'
+
+        #
+        # The database connection configuration.
+        #
+        # @return [Hash{Symbol => String,Integer}]
+        #
+        def config
+          if options[:db_uri]
+            parse_uri(options[:db_uri])
+          else
+            DB.config[options[:db]]
+          end
+        end
+
+        #
+        # Connects to the database.
+        #
+        def connect
+          ActiveRecord::Base.establish_connection(config)
+        end
+
+      end
+    end
+  end
+end
---
+++ spec/cli/database_command_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+require 'ronin/db/cli/database_command'
+
+describe Ronin::DB::CLI::DatabaseCommand do
+  describe "#config" do
+    context "when neither options[:db] nor options[:db_uri] are set" do
+      it "must default to connecting to the :default database" do
+        expect(subject.config).to eq(Ronin::DB.config[:default])
+      end
+    end
+
+    context "when options[:db] is set" do
+      let(:db_name) { :test }
+      let(:config) do
+        {
+          db_name => {
+            adapter: 'sqlite3',
+            database: '/path/to/db.sqlite3'
+          }
+        }
+      end
+
+      before do
+        allow(Ronin::DB).to receive(:config).and_return(config)
+
+        subject.options[:db] = db_name
+      end
+
+      it "must return the database configuration for options[:db]" do
+        expect(subject.config).to eq(Ronin::DB.config[db_name])
+      end
+    end
+
+    context "when options[:db_uri] is set" do
+      let(:db_uri)     { "postgres://user:password@host:1234/database" }
+      let(:parsed_uri) { subject.parse_uri(subject.options[:db_uri])   }
+
+      before { subject.options[:db_uri] = db_uri }
+
+      it "must return the parsed URI for options[:db_uri]" do
+        expect(subject.config).to eq(parsed_uri)
+      end
+    end
+  end
+
+  describe "#connect" do
+    it "must call ActiveRecord::Base.establish_connection with #config" do
+      expect(ActiveRecord::Base).to receive(:establish_connection).with(subject.config)
+
+      subject.connect
+    end
+  end
+end
