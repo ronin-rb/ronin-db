9c9fe4200392291952ff688b39bfb2d80861c6c1
Postmodern <postmodern.mod3@gmail.com>
2022-08-29 21:19:47 -0700

    Added a `ronin-db remove` command (closes #28).

---
+++ lib/ronin/db/cli/commands/remove.rb
@@ -0,0 +1,68 @@
+#
+# ronin-db - A common database library for managing and querying security data.
+#
+# Copyright (c) 2006-2022 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# ronin-db is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-db is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-db.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'ronin/db/cli/command'
+require 'ronin/db/config_file'
+
+module Ronin
+  module DB
+    class CLI
+      module Commands
+        #
+        # Removes an entry from the `database.yml` config file.
+        #
+        # ## Usage
+        #
+        #     ronin-db remove [options] NAME
+        #
+        # ## Options
+        #
+        #     -h, --help                       Print help information
+        #
+        # ## Arguments
+        #
+        #     NAME                             The DB name to remove
+        #
+        class Remove < Command
+
+          usage '[options] NAME'
+
+          argument :name, desc: 'The DB name to remove'
+
+          description 'Removed a database from the configuration file'
+
+          man_page 'ronin-db-remove.1'
+
+          #
+          # Runs the `ronin-db remove` command.
+          #
+          # @param [String] name
+          #   The database name to remove.
+          #
+          def run(name)
+            ConfigFile.edit do |config|
+              config.delete(name.to_sym)
+            end
+          end
+
+        end
+      end
+    end
+  end
+end
---
+++ man/ronin-db-remove.1.md
@@ -0,0 +1,42 @@
+# ronin-db-remove 1 "2022-01-01" Ronin DB "User Manuals"
+
+## SYNOPSIS
+
+`ronin-db remove` [*options*] *NAME*
+
+## DESCRIPTION
+
+Removes a database entry from the `~/.config/ronin-db/database.yml`
+configuration file.
+
+## ARGUMENTS
+
+*NAME*
+  The database entry to remove.
+
+## OPTIONS
+
+`-h`, `--help`
+  Print help information.
+
+## FILES
+
+*~/.config/ronin-db/database.yml*
+	The `ronin-db` database(s) configuration file.
+
+## ENVIRONMENT
+
+HOME
+	Specifies the home directory of the user. Ronin will search for the
+	*~/.config/ronin-db* cache directory within the home directory.
+
+XDG_CONFIG_HOME
+  Specifies the cache directory to use. Defaults to *$HOME/.config*.
+
+## AUTHOR
+
+Postmodern <postmodern.mod3@gmail.com>
+
+## SEE ALSO
+
+ronin-db(1) ronin-db-add(1) ronin-db-list(1) ronin-db-edit(1)
