c3c53a9f8bbe326cbd133ebf92487c1d589b0125
Postmodern <postmodern.mod3@gmail.com>
2022-08-29 23:07:25 -0700

    Use `ActiveRecord::Base.establish_connection` in `spec_helper`.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,14 @@ require 'rspec'
 require 'simplecov'
 SimpleCov.start
 
-require 'ronin/db'
-include Ronin::DB
+require 'active_record'
+ActiveRecord::Base.establish_connection(
+  adapter: 'sqlite3', database: ':memory:'
+)
 
-Ronin::DB.connect({adapter: 'sqlite3', database: ':memory:'})
+RSpec.configure do |specs|
+  specs.before(:suite) do
+    require 'ronin/db/migrations'
+    Ronin::DB::Migrations.migrate
+  end
+end
