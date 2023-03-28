00458bdcc3a4a4499acc5325c525fd881b68d211
Postmodern <postmodern.mod3@gmail.com>
2023-03-28 00:49:07 -0700

    Added `Ronin::DB::Tasks`.

---
+++ lib/ronin/db/tasks.rb
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+#
+# ronin-db - A common database library for managing and querying security data.
+#
+# Copyright (c) 2006-2023 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# ronin-db is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-db is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-db.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'rake/tasklib'
+
+module Ronin
+  module DB
+    #
+    # Defines `ronin:db` tasks in a `Rakefile`.
+    #
+    # ## Example
+    #
+    #     require 'ronin/db/tasks'
+    #     Ronin::DB::Tasks.new
+    #
+    class Tasks < Rake::TaskLib
+
+      #
+      # Initializes the tasks.
+      #
+      # @api public
+      #
+      def initialize
+        define
+      end
+
+      #
+      # Defines the `ronin:db` tasks.
+      #
+      # @api private
+      #
+      def define
+        namespace :db do
+          task :connect do
+            require 'ronin/db'
+            require 'logger'
+            DB.logger = Logger.new($stderr,:debug)
+            DB.connect
+          end
+
+          desc 'Migrates the development database'
+          task :migrate => :connect do
+            require 'ronin/db/migrations'
+            DB::Migrations.up
+          end
+
+          desc 'Starts an interactive database console'
+          task :console => :connect do
+            require 'ronin/db/cli/ruby_shell'
+            DB::CLI::RubyShell.start
+          end
+        end
+      end
+
+    end
+  end
+end
