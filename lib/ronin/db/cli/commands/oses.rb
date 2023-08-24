bd05da337331df1d3bc1a3c7c0d4f7a516c00192
Postmodern <postmodern.mod3@gmail.com>
2023-08-24 04:29:18 -0700

    Added the `ronin-db oses` command (closes #45).

---
+++ lib/ronin/db/cli/commands/oses.rb
@@ -0,0 +1,94 @@
+# frozen_string_literal: true
+#
+# ronin-db - A common database library for managing and querying security data.
+#
+# Copyright (c) 2006-2023 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# ronin-db is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-db is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-db.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'ronin/db/cli/model_command'
+
+module Ronin
+  module DB
+    class CLI
+      module Commands
+        #
+        # Queries all OSes in the database.
+        #
+        # ## Usage
+        #
+        #     ronin-db oses [options]
+        #
+        # ## Options
+        #
+        #         --db NAME                    The database to connect to (Default: default)
+        #         --db-uri URI                 The database URI to connect to
+        #     -v, --verbose                    Enables verbose output
+        #     -N, --name NAME                  Searches for all OSes records with the matching name
+        #         --named NAME                 Searches for all OSes containing the NAME
+        #     -F, --flavor linux|bsd           Searches for all OSes with the flavor
+        #     -V, --version VERSION            Searches for all OSes with the VERSION
+        #     -h, --help                       Print help information
+        #
+        # @since 0.2.0
+        #
+        class Oses < ModelCommand
+
+          model_file 'ronin/db/os'
+          model_name 'OS'
+
+          option :name, short: '-N',
+                        value: {
+                          type:  String,
+                          usage: 'NAME'
+                        },
+                        desc: 'Searches for all OSes records with the matching name' do |name|
+                          @query_method_calls << [:with_name, [name]]
+                        end
+
+          option :named, value: {
+                           type:  String,
+                           usage: 'NAME'
+                         },
+                         desc: 'Searches for all OSes containing the NAME' do |name|
+                           @query_method_calls << [:named, [name]]
+                         end
+
+          option :flavor, short: '-F',
+                          value: {
+                            type: [:linux, :bsd]
+                          },
+                          desc: 'Searches for all OSes with the flavor' do |flavor|
+                            @query_method_calls << [:with_flavor, [flavor]]
+                          end
+
+          option :version, short: '-V',
+                           value: {
+                             type:  String,
+                             usage: 'VERSION'
+                           },
+                           desc: 'Searches for all OSes with the VERSION' do |version|
+                             @query_method_calls << [:with_version, [version]]
+                           end
+
+          description 'Queries OSes'
+
+          man_page 'ronin-db-oses.1'
+
+        end
+      end
+    end
+  end
+end
---
+++ man/ronin-db-oses.1.md
@@ -0,0 +1,63 @@
+# ronin-db-oses 1 "2023-02-01" Ronin "User Manuals"
+
+## SYNOPSIS
+
+`ronin-db oses` [*options*]
+
+## DESCRIPTION
+
+Queries OSes in the database.
+
+## OPTIONS
+
+`--db` *NAME*
+  The database name to connect to. Defaults to `default` if not given.
+
+`--db-uri` *URI*
+  The explicit database URI to connect to.
+
+  * **sqlite3**: `sqlite3:relative/path.db` or `sqlite3:///absolute/path.db`
+  * **mysql**: `mysql://user:password@host/database`
+  * **postgres**: `postgres://user:password@host/database`
+
+`-v`, `--verbose`
+  Enables verbose output
+
+`-N`, `--name` *NAME*
+  Searches for all OSes with the matching name.
+
+`--named` *NAME*
+  Searches for all OSes containing the name.
+
+`-F`, `--flavor` `linux`\|`bsd`
+  Searches for all OSes with the matching flavor.
+
+`-V`, `--version` *VERSION*
+  Searches for all OSes with the matching version.
+
+`-h`, `--help`
+  Print help information.
+
+## ENVIRONMENT
+
+*HOME*
+  Alternate location for the user's home directory.
+
+*XDG_CONFIG_HOME*
+  Alternate location for the `~/.config` directory.
+
+*XDG_DATA_HOME*
+  Alternate location for the `~/.local/share` directory.
+
+## FILES
+
+`~/.local/share/ronin-db/database.sqlite3`
+  The default sqlite3 database file.
+
+`~/.config/ronin-db/database.yml`
+  Optional database configuration.
+
+## AUTHOR
+
+Postmodern <postmodern.mod3@gmail.com>
+
